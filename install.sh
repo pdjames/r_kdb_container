d03c7248abd3cd0cb4853c2e09344f232468f439
pdjames <pjamesis@gmail.com>
2023-04-21 15:10:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 while IFS=" " read $1 -r package version;
 do
-  R -e "if('"$package"' %in% rownames(installed.packages()) == FALSE) {install.packages('https://cran.r-project.org/package="$package"&version="$version"', dependencies=FALSE , repos=NULL, type='source')}";
-#  Rscript -e "if('"$package"' %in% rownames(installed.packages()) == FALSE) {install.packages('"$package"', dependencies=TRUE)}";
-#  R -e "install.packages('"$package"', dependencies=TRUE, repos='http://cran.rstudio.com/')";
-#  R -e "devtools::install_version('"$package"', dependencies=TRUE, repos='http://cran.rstudio.com/')";
+  R -e "if('"$package"' %in% rownames(installed.packages()) == FALSE) {install.packages('https://cran.r-project.org/package="$package"&version="$version"', quiet=TRUE, dependencies=FALSE , repos=NULL, type='source')}";
 done
