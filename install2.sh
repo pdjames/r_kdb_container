3014a9a1d0584299e69dc536750b474cbfb5fa89
pdjames <pjamesis@gmail.com>
2023-05-04 15:29:00 +0100

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+while IFS=" " read $1 -r package version;
+do
+  R -e "if('"$package"' %in% rownames(installed.packages()) == FALSE) {install.packages('https://cran.r-project.org/package="$package"', dependencies=FALSE , repos=NULL, type='source')}";
+done
